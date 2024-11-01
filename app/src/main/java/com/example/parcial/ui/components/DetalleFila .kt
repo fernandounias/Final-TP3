c51e52cb463a8b04bddf755ef880213c05287d98
augustodoldan <augusto.doldan@hotmail.com>
2024-11-01 01:09:26 -0300

    WIP: Se crea componente y se avanza con los estilos

---
+++ app/src/main/java/com/example/parcial/ui/components/DetalleFila .kt
@@ -0,0 +1,74 @@
+package com.example.parcial.ui.components
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun DetalleFila (
+    fecha: String, descripcion: String, monto: Double, autorizacion: String, modifier: Modifier
+) {
+
+// @TODO AUGUSTO: Cambiar colores hardcodeados por colores de MaterialTheme y el font family.
+    val textColor = if (monto > 0) Color(0xFF00A067) else Color(0xFFF96440)
+    val formattedMonto = if (monto > 0) "+$%.2f".format(monto) else "$%.2f".format(monto)
+
+    val customTextStyle = TextStyle(
+        fontFamily = FontFamily.SansSerif,
+        fontSize = 14.sp,
+        fontWeight = FontWeight.Medium,
+        lineHeight = 19.6.sp,
+        textAlign = TextAlign.Left
+    )
+
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(IntrinsicSize.Min)
+            .sizeIn(minWidth = 360.dp, minHeight = 56.dp)
+            .border(
+                width = 1.dp,
+                color = Color.Gray.copy(alpha = 0.1f),
+                shape = RoundedCornerShape(1.dp)
+            )
+            .padding(top = 8.dp, end = 12.dp, bottom = 8.dp, start = 12.dp),
+    ) {
+        Text(
+            text = fecha, style = customTextStyle, color = Color.Black
+        )
+        Spacer(modifier = Modifier.weight(0.05f))
+        Column {
+            Text(
+                text = descripcion,
+                style = customTextStyle,
+            )
+            Text(
+                text = autorizacion,
+                style = customTextStyle,
+            )
+        }
+        Spacer(modifier = Modifier.weight(0.2f))
+        Text(
+            text = formattedMonto,
+            style = customTextStyle,
+            color = textColor,
+        )
+    }
+}
