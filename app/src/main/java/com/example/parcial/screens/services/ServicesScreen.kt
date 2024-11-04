ef9ec6e87d79c277ad4116e335caaa4512c640ef
Manu312 <manumercado14@gmail.com>
2024-11-03 23:13:21 -0300

    Intetamos hacer responsive ScreenServices

--- app/src/main/java/com/example/parcial/screens/services/ServicesScreen.kt
+++ app/src/main/java/com/example/parcial/screens/services/ServicesScreen.kt
@@ -2,11 +2,8 @@ package com.example.parcial.screens.services
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -15,7 +12,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
@@ -32,9 +28,6 @@ fun ServicesScreen(
 ) {
     val manropeBold = FontFamily(
         Font(R.font.manrope_bold)
-    )
-    val manropeRegular = FontFamily(
-        Font(R.font.manrope_regular)
     )
     Column(
         modifier = Modifier
