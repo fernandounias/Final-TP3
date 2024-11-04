7ea08ccecfb1ea750201175653f4a214928ae604
Manu312 <manumercado14@gmail.com>
2024-11-04 13:44:44 -0300

    Agregamos SubeDialogs de la Screen Services

--- app/src/main/java/com/example/parcial/MainActivity.kt
+++ app/src/main/java/com/example/parcial/MainActivity.kt
@@ -5,8 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,18 +14,9 @@ import com.example.parcial.ui.theme.ParcialTheme
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.parcial.navigation.MainNavGraph
 import androidx.compose.material3.DrawerValue
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.parcial.navigation.RootScreen
@@ -107,7 +96,6 @@ fun ScaffoldContent(
         MainNavGraph(
             startDestination = RootScreen.Splash.route,
             navController = navController,
-            navigationActions = navigationActions,
             modifier = Modifier.padding(innerPadding)
         )
     }
--- app/src/main/java/com/example/parcial/navigation/MainNavGraph.kt
+++ app/src/main/java/com/example/parcial/navigation/MainNavGraph.kt
@@ -2,13 +2,10 @@ package com.example.parcial.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navigation
 import com.example.parcial.MainNavActions
 import com.example.parcial.screens.account.AccountScreen
 import com.example.parcial.screens.card.CardScreen
@@ -16,7 +13,6 @@ import com.example.parcial.screens.home.HomeScreen
 import com.example.parcial.screens.login.LoginScreen
 import com.example.parcial.screens.profile.ProfileScreen
 import com.example.parcial.screens.services.ServicesScreen
-import com.example.parcial.screens.services.ServicesSubeScreen
 import com.example.parcial.screens.splash.SplashScreen
 
 @Composable
@@ -24,15 +20,14 @@ fun MainNavGraph(
     modifier: Modifier = Modifier,
     navController: NavHostController,
 //    viewModel: MainActivityViewModel,
-    startDestination: String = RootScreen.Splash.route,
-    navigationActions: MainNavActions
+    startDestination: String = RootScreen.Splash.route
 ) {
     NavHost(
         navController = navController,
         startDestination = startDestination,
         modifier = modifier
     ){
-        addSplashRoute(navController, navigationActions)
+        addSplashRoute(navController)
         addLoginRoute()
         addHomeRoute()
         addAccountRoute()
@@ -43,8 +38,7 @@ fun MainNavGraph(
 
 }
 fun NavGraphBuilder.addSplashRoute(
-    navController: NavHostController,
-    navigationActions: MainNavActions
+    navController: NavHostController
 ) {
     composable(RootScreen.Splash.route) {
         SplashScreen(navController)
@@ -79,19 +73,7 @@ fun NavGraphBuilder.addPerfilRoute(){
     }
 }
 fun NavGraphBuilder.addServicesRoutes() {
-    navigation(
-        route = RootScreen.Services.route,
-        startDestination = LeafScreen.Services.route
-    ) {
         composable(LeafScreen.Services.route) {
             ServicesScreen()
         }
-        composable(LeafScreen.Sube.route) {
-            ServicesSubeScreen()
-        }
-//        composable(LeafScreen.SubeSucces.route) {
-//            ServicesSubeSuccesScreen(navController, navigationActions)
-//        }
-
-    }
 }
