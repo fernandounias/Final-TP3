e5c94220384f259abaa063c21be154d0a876db6b
fernandounias <44074668+fernandounias@users.noreply.github.com>
2024-11-27 19:52:10 -0300

    Replace RetrofitModule with a Hilt module

---
+++ app/src/main/java/com/example/parcial/shared/infraestructure/NetworkModule.kt
@@ -0,0 +1,41 @@
+package com.example.parcial.shared.infraestructure
+
+import com.example.parcial.shared.infraestructure.Auth.AuthServices
+import com.example.parcial.shared.infraestructure.users.UserServices
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import jakarta.inject.Singleton
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    private const val BASE_URL = "https://fakestoreapi.com/"
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideAuthServices(retrofit: Retrofit): AuthServices {
+        return retrofit.create(AuthServices::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideUserServices(retrofit: Retrofit): UserServices {
+        return retrofit.create(UserServices::class.java)
+    }
+
+    //replaces RetrofitModule object and implemets its function with hilt
+}
